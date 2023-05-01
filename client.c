191b39a0e208e961597c77dd00103251391eb449
Maya Knotts <knotts@royal-05.cs.wisc.edu>
2023-05-01 18:19:44 -0500

    attempt at server.c

--- client.c
+++ client.c
@@ -3,9 +3,10 @@
 #include <stdlib.h> // exit(0);
 #include <unistd.h> // close
 #include <sys/time.h> //timeval
-#include <client.h>
-#include <socket.h>
-#include <message.h>
+#include <sys/socket.h>
+#include "udp.h"
+#include "rpc.h"
+#include "client.h"
 
 // initializes the RPC connection to the server
 struct rpc_connection RPC_init(int src_port, int dst_port, char dst_addr[]){
@@ -13,14 +14,14 @@ struct rpc_connection RPC_init(int src_port, int dst_port, char dst_addr[]){
     struct socket recv_socket = init_socket(src_port); 
 
     // populate socket address using
-    struct socketaddr_storage dst = malloc(sizeof(socketaddr_storage)); // is malloc necessary?
-    socklen_t addrlen = 0;
+    struct socketaddr_storage dst; 
+    socklen_t addrlen;
     populate_sockaddr(AF_INET, dst_port, dst_addr, dst, addrlen);
 
     // initialize and set rpc_connection arguments
-    struct rpc_connection rpc = malloc(sizeof(rpc_connection));
+    struct rpc_connection rpc = malloc(sizeof(struct rpc_connection));
     rpc.recv_socket = recv_socket;
-    rpc.dst_addr = *((struct sockaddr *)(&addr));
+    rpc.dst_addr = *((struct sockaddr *)(&dst));
     rpc.dst_len = addrlen;
     rpc.seq_number = 0;
     rpc.client_id = rand();
@@ -29,15 +30,15 @@ struct rpc_connection RPC_init(int src_port, int dst_port, char dst_addr[]){
 
 // Sleeps the server thread for a few seconds
 void RPC_idle(struct rpc_connection *rpc, int time){
-    struct message m = malloc(sizeof(message));
-    m.fxn = "idl";
-    m.arg1 = time;
-    m.arg2 = -1;
+    struct message * m = malloc(sizeof(struct message));
+    m->fxn = "idl";
+    m->arg1 = time;
+    m->arg2 = -1;
     struct packet_info pi;
     for (int i = 0; i < RETRY_COUNT; i++){
         send_packet(rpc->recv_socket, rpc->dst_addr, rpc->dst_len, (char *) m, sizeof(message));
-        pi = receive_packet_timeout(TIMEOUT_TIME);
-        if (packet_info.recv_len != 0){
+        pi = receive_packet_timeout(rpc->recv_socket, TIMEOUT_TIME);
+        if (pi.recv_len != 0){
             return 0;
         }
     }
@@ -47,15 +48,15 @@ void RPC_idle(struct rpc_connection *rpc, int time){
 
 // gets the value of a key on the server store
 int RPC_get(struct rpc_connection *rpc, int key){
-    struct message m = malloc(sizeof(message));
-    m.fxn = "get";
-    m.arg1 = key;
-    m.arg2 = -1;
+    struct message * m = malloc(sizeof(struct message));
+    m->fxn = "get";
+    m->arg1 = key;
+    m->arg2 = -1;
     struct packet_info pi;
     for (int i = 0; i < RETRY_COUNT; i++){
         send_packet(rpc->recv_socket, rpc->dst_addr, rpc->dst_len, (char *) m, sizeof(message));
-        pi = receive_packet_timeout(TIMEOUT_TIME);
-        if (packet_info.recv_len != 0){
+        pi = receive_packet_timeout(rpc->recv_socket, TIMEOUT_TIME);
+        if (pi.recv_len != 0){
             int ret = atoi(pi.buf);
             return ret;
         }
@@ -66,15 +67,15 @@ int RPC_get(struct rpc_connection *rpc, int key){
 
 // sets the value of a key on the server store
 int RPC_put(struct rpc_connection *rpc, int key, int value){
-    struct message m = malloc(sizeof(message));
-    m.fxn = "put";
-    m.arg1 = key;
-    m.arg2 = value;
+    struct message * m = malloc(sizeof(struct message));
+    m->fxn = "put";
+    m->arg1 = key;
+    m->arg2 = value;
     struct packet_info pi;
     for (int i = 0; i < RETRY_COUNT; i++){
         send_packet(rpc->recv_socket, rpc->dst_addr, rpc->dst_len, (char *) m, sizeof(message));
-        pi = receive_packet_timeout(TIMEOUT_TIME);
-        if (packet_info.recv_len != 0){
+        pi = receive_packet_timeout(rpc->recv_socket, TIMEOUT_TIME);
+        if (pi.recv_len != 0){
             int ret = atoi(pi.buf);
             return ret;
         }
--- server.c
+++ server.c
@@ -3,3 +3,45 @@
 #include <stdlib.h> // exit(0);
 #include <unistd.h> // close
 #include <sys/time.h> //timeval
+#include <pthread.h>
+#include "rpc.h"
+#include "server_functions.h"
+
+struct client{
+    int clientID;
+    int seqNum;
+    int lastRes;
+};
+
+struct socket s;
+
+void * call_function(void * arg){
+    struct packet_info pi = *((struct packet_info *) arg);
+	struct message * m = (struct message *) (pi.buf);
+    char * ret = malloc(sizeof(int));
+
+    if (strcmp(m->fxn, "idl") == 0){
+        idle(m->arg1);
+        sprintf(ret, "%d", 0);
+    } else if (strcmp(m->fxn, "put") == 0){
+        int res = put(m->arg1, m->arg2);
+        sprintf(ret, "%d", res);
+    } else if (strcmp(m->fxn, "get") == 0){
+        int res = get(m->arg1);
+        sprintf(ret, "%d", res);
+    }
+    send_packet(s, pi.sock, pi.recv_len, ret, sizeof(int));
+    void * r = malloc(sizeof(1));
+    return r;
+}
+
+int main(){
+    s = init_socket(SERVER_PORT);
+    while (1){
+        struct packet_info pi = receive_packet(s);
+        if (pi.recv_len != 0){
+            pthread_t * child_thread = malloc(sizeof(child_thread));
+            pthread_create(child_thread, NULL, call_function, &pi);
+        }
+    }
+}
